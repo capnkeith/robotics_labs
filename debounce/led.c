d8108bf014d4c937cfeb663212c2f9b591f7d434
Seth Keith <seth@ncheese-2.local>
2021-09-19 17:41:13 -0700

    whitespace

--- debounce/led.c
+++ debounce/led.c
@@ -31,8 +31,8 @@
 // these problems by introducing state machine programming.
 //
 //******************************************************************************
-#include <avr/io.h> 		// include to access port IO
-#include <avr/interrupt.h>	// include for using timers.
+#include <avr/io.h>             // include to access port IO
+#include <avr/interrupt.h>      // include for using timers.
 
 volatile uint32_t ticks=0;
 uint8_t steps[] = { 1,2,4,8 };
@@ -41,13 +41,13 @@ uint8_t steps[] = { 1,2,4,8 };
 //  start a timer interrupt periodically. 
 void init_timer(void)
 {
-    cli();			// disable interrupts during timer init.
-    OCR2A = 62;			// compare against 
+    cli();                      // disable interrupts during timer init.
+    OCR2A = 62;                 // compare against 
     TCCR2A |= (1 << WGM21);     // CTC mode
     TIMSK2 |= (1 << OCIE2A);    // enable timer compare interrupt
-    TCCR2B |= (1 << CS21);	// prescaler to 128
-    ticks = 0;			// init ticks to 0
-    sei();			// enable interrupts. Timer is now running.
+    TCCR2B |= (1 << CS21);      // prescaler to 128
+    ticks = 0;                  // init ticks to 0
+    sei();                      // enable interrupts. Timer is now running.
 }
 
 // This is an interrupt service routine. It will be called every few
@@ -57,47 +57,47 @@ void init_timer(void)
 // volatile.
 ISR (TIMER2_COMPA_vect)
 {
-    ticks++;	     	        	// add 1 to ticks each time called.
+    ticks++;                            // add 1 to ticks each time called.
 }
 
 
-int main(void)     		
+int main(void)                  
 {                   
-    DDRB = 0b00001111;          		// B0, B1, B2, B3 are outputs, B6 is our input.
-    init_timer();               		// this starts the ISR running periodically.
-    uint32_t last_tick=ticks;   		// last_tick records elapsed ticks
-    uint8_t step = 0;           		// step is the LED in steps[] to light
-    uint8_t event_triggered = 0;		// track if a button press has been triggered.
-    uint8_t last_button = 0;			// track the last input value read.
+    DDRB = 0b00001111;                          // B0, B1, B2, B3 are outputs, B6 is our input.
+    init_timer();                               // this starts the ISR running periodically.
+    uint32_t last_tick=ticks;                   // last_tick records elapsed ticks
+    uint8_t step = 0;                           // step is the LED in steps[] to light
+    uint8_t event_triggered = 0;                // track if a button press has been triggered.
+    uint8_t last_button = 0;                    // track the last input value read.
     while(1)
     {
-        while (last_tick==ticks); 		// wait till the next tick.
-        last_tick = ticks;			// update last_tick current ticks
+        while (last_tick==ticks);               // wait till the next tick.
+        last_tick = ticks;                      // update last_tick current ticks
 
-        uint8_t button_in = PINB & (1<<6); 	// read PIN B6 into button_in
-        if (button_in != last_button)		// if button_in changed last tic
+        uint8_t button_in = PINB & (1<<6);      // read PIN B6 into button_in
+        if (button_in != last_button)           // if button_in changed last tic
         {
-            last_button = button_in;		// record the new value in last_button
-            event_triggered = 0;		// reset the even trigger
+            last_button = button_in;            // record the new value in last_button
+            event_triggered = 0;                // reset the even trigger
         }
-        else 					// otherwise button same for 2 ticks
+        else                                    // otherwise button same for 2 ticks
         {
-            if (!event_triggered)		// if the event was not triggered yet
+            if (!event_triggered)               // if the event was not triggered yet
             {
-                event_triggered = 1;		// trigger the event. 
-		if (button_in)			// if the button is pressed
- 		{
-                    step++;			// then go to the next step
+                event_triggered = 1;            // trigger the event. 
+                if (button_in)                  // if the button is pressed
+                {
+                    step++;                     // then go to the next step
                     if (step == sizeof(steps))  // if past the last step
-	                step = 0;               // go back to step 0.
-		}
-		else				// otherwise this is a button release
-		{
-	            			        // if you want a button relese action do it here.
-		}
+                        step = 0;               // go back to step 0.
+                }
+                else                            // otherwise this is a button release
+                {
+                                                // if you want a button relese action do it here.
+                }
             }
         }
-        PORTB = steps[step];			// light the LEDs with the current step.
+        PORTB = steps[step];                    // light the LEDs with the current step.
     }
 }
 
